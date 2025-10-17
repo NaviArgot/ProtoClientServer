80772494c964f8746eb1353bd67840ebb249d52e
Usu <=>
2025-10-16 21:26:08 -0600

    Changed main to conform to dependency injection pattern used by network modules

--- main.lua
+++ main.lua
@@ -1,30 +1,41 @@
 local Server = require("network.Server")
 local Client = require("network.Client")
-local GQ = require("game.GlobalQueues")
+local queue = require("modules.queue")
 local Console = require('modules.Console')
 
 local isServer
 local isConsoleVisible = true
 local console = Console.create()
 local network
+local messageQueue, actionsQueue, responsesQueue
 
 function love.load (args)
-    print("")
-    print("-1 % 10 = ", -1 % 10)
-    print(" 9 % 10 = ",  9 % 10)
+    messageQueue = queue.new()
+    actionsQueue = queue.new()
+    responsesQueue = queue.new()
     if args[1] == "--server" then
-        network = Server.create()
+        network = Server.create(
+            messageQueue,
+            actionsQueue,
+            responsesQueue
+        )
         isServer = true
-    else network = Client.create() end
+    else 
+        network = Client.create(
+            messageQueue,
+            actionsQueue,
+            responsesQueue
+        )
+    end
     network:start()
 end
 
 function love.update ()
     network:listen()
-    local val = GQ.messageQueue:pop()
+    local val = messageQueue:pop()
     while val do
         console:add(val)
-        val = GQ.messageQueue:pop()
+        val = messageQueue:pop()
     end
     -- Exit love2d
     if love.keyboard.isDown("escape") then
